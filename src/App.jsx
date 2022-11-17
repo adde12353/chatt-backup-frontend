ec23fc9c068147d2cbd6c3d6344ed0c464fc0f24
adde12353 <39116019+adde12353@users.noreply.github.com>
2022-11-17 09:23:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    socket = io("https://osuka-chatt-api.herokuapp.com");
+    socket = io("https://backendchathehe.herokuapp.com/");
 
     socket.on("connect", () => {
       console.log("Connected");
